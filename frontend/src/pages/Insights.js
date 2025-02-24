df0aba4b940e1a4421f963f3cc7961b0ca7dadbb
Silver094 <deepanshuvishwakarma14@gmail.com>
2025-02-24 23:00:00 +0530

    little frontend changes

--- frontend/src/pages/Insights.js
+++ frontend/src/pages/Insights.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
 import { FaLightbulb } from "react-icons/fa";
-
+import useAxios from "../hooks/useAxios";
 const Insights = () => {
+  const { fetchData } = useAxios();
+  // useEffect(() => {
+  //   const getHabits = async () => {
+  //     const response = await fetchData({
+  //       method: "GET",
+  //       url: "/api/insights",
+  //       auth: true,
+  //     });
+  //     if (response.data) {
+  //       setHabits(response.data);
+  //     } else {
+  //       console.error("Error fetching habits:", response.error);
+  //     }
+  //   };
+
+  //   getHabits();
+  //   // eslint-disable-next-line
+  // }, []);
   return (
     <Card className="p-4 shadow">
       <h4>AI-Powered Insights</h4>
       <p>
-        Get personalized habit recommendations based on your consistency and performance.
+        Get personalized habit recommendations based on your consistency and
+        performance.
       </p>
       <Button variant="info">
         <FaLightbulb /> View Detailed Insights
